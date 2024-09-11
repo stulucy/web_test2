fbbe05a8a482ede2911f2d1bf78e172e56cfe278
stulucy <syd4975@naver.com>
2024-09-11 21:58:32 +0900

    20240911 서예닮

---
+++ workspace_html/task01.js
@@ -0,0 +1,14 @@
+const task_button=document.querySelector(".task_button");
+const task=document.querySelector(".tasks-2");
+task_button.addEventListener('click', () => {
+   	const add_task=prompt("추가할 할일을 입력하세요");
+
+		with(task.style){
+		  height="200px;"
+			// border="2px dashed blue";
+			width="100px";
+			// backgroundColor="skyblue";
+		
+		}
+});
+
